5da7f5a890b62bc6156c0a0f7e90ecc5589b0665
ljssc <igor.salnikov.00@bk.ru>
2026-02-02 13:00:15 +0500

    Add files via upload

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import '../scss/styles.scss'
+
+import * as bootstrap from 'bootstrap'
+
+
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
